2fecde268f727ee241e1ef1c8c1e4ca5b5b03407
Joshua Richardson <joshua.richardson@myunidays.com>
2022-02-11 14:31:40 +0000

    Added lesson 4 and 5

---
+++ lesson-04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Amazon;
+using Amazon.EC2;
+using Amazon.Runtime.CredentialManagement;
+
+namespace lesson_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chain = new CredentialProfileStoreChain("C:\\Users\\joshua.richardson\\.aws\\credentials");
+            chain.TryGetAWSCredentials("associate-developer", out var credentials);
+
+            var ec2 = new AmazonEC2Client(credentials, RegionEndpoint.EUWest2);
+
+            var instanceDescription = ec2.DescribeInstancesAsync().Result;
+
+            if (!instanceDescription.Reservations.Any())
+            {
+                Console.WriteLine("No reservations");
+                return;
+            }
+
+            foreach (var reservation in instanceDescription.Reservations)
+            {
+                var instances = reservation.Instances;
+
+                if (!instances.Any())
+                    continue;
+
+                foreach (var instance in instances)
+                {
+                    var name = instance.Tags.FirstOrDefault(x => x.Key == "Name")?.Value;
+                    Console.WriteLine($"Name: {name}, AZ: {instance.Placement.AvailabilityZone}, State: {instance.State}");
+                }
+            }
+        }
+    }
+}
---
+++ lesson-05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace lesson_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s3 = new AmazonS3Client();
+            var myObject = s3.GetObjectAsync(new GetObjectRequest
+            {
+                Key = "s3://adventures-of-wilbur-images/Wilbur_2020_8_15_16_37_23_29.jpg",
+                BucketName = "adventures-of-wilbur-images"
+            }).Result;
+
+            Console.WriteLine($"Status: {myObject.HttpStatusCode}");
+        }
+    }
+}
